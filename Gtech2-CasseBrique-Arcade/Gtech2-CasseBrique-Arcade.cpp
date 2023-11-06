33fbcb5d59c4aaaee3cb175d5158ee93c49e4cae
doriansimonet <dsimonet@gaming.tech>
2023-11-06 12:16:24 +0100

    Ajoutez des fichiers projet.

---
+++ Gtech2-CasseBrique-Arcade/Gtech2-CasseBrique-Arcade.cpp
@@ -0,0 +1,47 @@
+
+#include <SFML/Graphics.hpp>
+
+int main(int argc, char** argv)
+{
+    //Création d'une fenêtre
+    sf::RenderWindow oWindow(sf::VideoMode(640, 480), "SFML");
+
+    //Création d'un cercle de radius 100
+    sf::CircleShape oCircle(100.f);
+    //A la position 0, 0
+    oCircle.setPosition(0.f, 0.f);
+    //Et de couleur verte
+    oCircle.setFillColor(sf::Color::Green);
+
+
+    //Création d'un rectangle de taille 50, 50
+    sf::RectangleShape oRectangle(sf::Vector2f(50.f, 50.f));
+    //A la position 100, 100
+    oRectangle.setPosition(100.f, 100.f);
+    //Et de couleur rouge
+    oRectangle.setFillColor(sf::Color::Red);
+
+    //GameLoop
+    while (oWindow.isOpen())
+    {
+        //EVENT
+        sf::Event oEvent;
+        while (oWindow.pollEvent(oEvent))
+        {
+            if (oEvent.type == sf::Event::Closed)
+                oWindow.close();
+        }
+
+        //UPDATE
+
+        //DRAW
+        oWindow.clear();
+
+        oWindow.draw(oCircle);
+        oWindow.draw(oRectangle);
+
+        oWindow.display();
+    }
+
+    return 0;
+}
